33a8e150f8581fa2e97831f75432cb2a9c3b14a8
kyoshidajp <claddvd@gmail.com>
2015-06-13 21:34:37 +0900

    モジュール配下に移動

--- lib/runners_update/runner.rb
+++ lib/runners_update/runner.rb
@@ -1,36 +1,39 @@
-# ランナークラス
-class Runner
-  attr_accessor :number, :name, :splits
-  def initialize(number, name)
-    @number = number
-    @name = name
-    @splits = []
+module RunnersUpdate
+
+  # ランナークラス
+  class Runner
+    attr_accessor :number, :name, :splits
+    def initialize(number, name)
+      @number = number
+      @name = name
+      @splits = []
+    end
+
+    # ポイントの追加
+    #
+    # @param [String] name 測定ポイント名
+    # @param [String] split スプリットタイム
+    # @param [String] lap ラップタイム
+    # @param [String] pass 通過タイム
+    def add_point(name, split, lap, pass)
+      @splits << Point.new(name, split, lap, pass)
+    end
   end
 
-  # ポイントの追加
-  #
-  # @param [String] name 測定ポイント名
-  # @param [String] split スプリットタイム
-  # @param [String] lap ラップタイム
-  # @param [String] pass 通過タイム
-  def add_point(name, split, lap, pass)
-    @splits << Point.new(name, split, lap, pass)
-  end
-end
-
-# 測定ポイントクラス
-class Point
-  attr_accessor :name, :split, :lap, :pass
-  # initialize
-  #
-  # @param [String] name 測定ポイント名
-  # @param [String] split スプリットタイム
-  # @param [String] lap ラップタイム
-  # @param [String] pass 通過タイム
-  def initialize(name, split, lap, pass)
-    @name = name
-    @split = split
-    @lap = lap
-    @pass = pass
+  # 測定ポイントクラス
+  class Point
+    attr_accessor :name, :split, :lap, :pass
+    # initialize
+    #
+    # @param [String] name 測定ポイント名
+    # @param [String] split スプリットタイム
+    # @param [String] lap ラップタイム
+    # @param [String] pass 通過タイム
+    def initialize(name, split, lap, pass)
+      @name = name
+      @split = split
+      @lap = lap
+      @pass = pass
+    end
   end
 end
